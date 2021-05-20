6b069a401629870fd22a820535e775f85136b45a
Bonsay Social <social@bonsay.no>
2021-05-20 11:17:58 +0200

    Remove js data

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,16 @@
 import LazyLoad from 'vanilla-lazyload';
+import Typed from 'typed.js';
 
 new LazyLoad();
+
+
+let typed_options = {
+    strings: ['Enebolig', 'eller','Fritidsbolig', 'Særegen beliggenhet', 'Rett ved stranda', 'Rett ved Ekrabukta','350m til Villa Malla', 'Ta fergen fra Oslo til Filtvet Brygge'],
+    typeSpeed: 50,
+    startDelay: 100,
+    backDelay: 1600,
+    backSpeed: 10,
+    loop: true
+};
+
+new Typed('#typed_title', typed_options);
